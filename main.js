072e52a3436ba2c6d4d6e7e3b98971a01cc312bb
jacobhusband <jakehusband2@gmail.com>
2022-10-08 11:29:42 -0700

    Solve range-bit-count challenge on codesignal

--- main.js
+++ main.js
@@ -1,14 +1,12 @@
-
-function solution(a) {
-  // make each dec to bin
-  // reverse the array and join
-  // make bin to dec
-  return parseInt(
-    a
-      .map((x) => ("00000000" + x.toString(2)).slice(-8))
-      .reverse()
-      .join(""),
-    2
-  );
+function solution(a, b) {
+  var arr = [];
+  for (var i = a; i <= b; i++) {
+    arr.push(i);
+  }
+  return arr
+    .map((x) => {
+      var str = x.toString(2);
+      return str.split("").reduce((x, y) => parseInt(x) + parseInt(y), 0);
+    })
+    .reduce((x, y) => x + y, 0);
 }
-
